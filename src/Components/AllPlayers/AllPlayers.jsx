99c649410ffab2a2ea0549b4b77ca8ba75895195
appoclayptor <shahreza1414@gmail.com>
2024-10-25 04:13:44 +0600

    Styled the card according to the figma file

--- src/Components/AllPlayers/AllPlayers.jsx
+++ src/Components/AllPlayers/AllPlayers.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import Player from '../Player/Player';
 
@@ -10,7 +9,7 @@ const AllPlayers = () => {
         .then(data=> setPlayers(data))
     }, []);
     return (
-        <div className='w-11/12 mx-auto'>
+        <div className='p-4 grid grid-cols-3 gap-6 w-11/12 mx-auto'>
             {
                 players.map((player) => <Player key={player.playerId} player={player}></Player>)
             }
--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -5,21 +5,19 @@ import { FaFlag } from "react-icons/fa";
 const Player = ({ player }) => {
     const { name, country, image, role, battingType, bowlingType, price } = player;
     return (
-        <div className='grid grid-cols-3 gap-6'>
-            <div className='w-[424px] h-[528px] border-2 p-6 rounded-xl '>
-                <img className='w-fit' src={image} alt="" />
-                <span className='flex items-center text-xl font-bold'><FaUser />{name}</span>
-                <div className='flex justify-between'>
-                    <span className='flex items-center'><FaFlag />{country}</span>
-                    <span className='border bg-slate-300 p-1 rounded-md'>{role}</span>
-                </div>
-                <hr />
-                <span className='flex justify-between'><p>Batting-Type:</p> {battingType}</span>
-                <span className='flex justify-between'><p>Bowlling-Type:</p> {bowlingType}</span>
-                <div className='flex justify-between'>
-                    <p>{price}</p>
-                    <button className='btn-sm rounded-lg border-2 hover:bg-primary'>Choose Player</button>
-                </div>
+        <div className='border-2 p-6 rounded-xl '>
+            <img className='w-fit rounded-xl shadow-lg mb-6' src={image} alt="" />
+            <span className='flex items-center text-xl font-bold mb-4'><FaUser />{name}</span>
+            <div className='flex justify-between mb-4'>
+                <span className='flex items-center gap-1'><FaFlag />{country}</span>
+                <span className='border bg-slate-300 p-1 rounded-md'>{role}</span>
+            </div>
+            <hr />
+            <span className='flex justify-between mb-4'><p className='font-semibold'>Batting-Type:</p> {battingType}</span>
+            <span className='flex justify-between mb-4'><p className='font-semibold'>Bowlling-Type:</p> {bowlingType}</span>
+            <div className='flex justify-between items-center'>
+                <p className='font-semibold'>Price: {price}$</p>
+                <button className='btn-sm rounded-lg border-2 hover:bg-primary'>Choose Player</button>
             </div>
         </div>
     );
