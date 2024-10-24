86853919b377e8fac483afb6f3f4415bdf6cbf15
appoclayptor <shahreza1414@gmail.com>
2024-10-24 22:51:32 +0600

    work in progress

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold text-green-400'>Working on the website</h1>
+    </>
+  )
+}
+
+export default App
