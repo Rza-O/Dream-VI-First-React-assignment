a1bd37bccddd88644ca5852e6b3c9c571c829673
appoclayptor <shahreza1414@gmail.com>
2024-10-26 19:28:18 +0600

    added price in the selected player section

--- src/Components/SingleSelected/SingleSelected.jsx
+++ src/Components/SingleSelected/SingleSelected.jsx
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 
 const SingleSelected = ({ sPlayer, deleteBtnHandler }) => {
     // console.log(sPlayer);
-    const { name, image, role } = sPlayer;
+    const { name, image, role, price } = sPlayer;
     return (
         <div className="flex justify-between p-6 border-2 rounded-xl items-center mt-4 mb-6">
-            <div className="flex gap-2">
-                <img className="w-20 h-16 rounded-xl object-cover" src={image} alt="" />
+            <div className="flex gap-4 items-center">
+                <img className="w-20 h-20 rounded-xl object-cover" src={image} alt="" />
                 <div className="space-y-3">
                     <h1 className="text-xl font-bold">{name}</h1>
                     <p className="text-sm text-gray-500">{role}</p>
+                    <p className="font-bold">Price: {price}💲</p>
                 </div>
             </div>
             <RiDeleteBin5Line onClick={() => deleteBtnHandler(sPlayer)}  className="text-red-400 cursor-pointer" />
